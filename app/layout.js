17de32837d4ae20d3ded540efe958147db6ed10a
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-07-14 03:00:57 +0530

    small chaned

--- app/layout.js
+++ app/layout.js
@@ -7,18 +7,20 @@ import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-	useEffect(() => {
-		if (process.env.NODE_ENV === "production") {
-			const script = document.createElement("script");
-			script.async = true;
-			script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9035732498574241";
-			script.crossOrigin = "anonymous";
-			document.head.appendChild(script);
-		}
-	}, [])
+	// useEffect(() => {
+	// 	if (process.env.NODE_ENV === "production") {
+	// 		const script = document.createElement("script");
+	// 		script.async = true;
+	// 		script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9035732498574241";
+	// 		script.crossOrigin = "anonymous";
+	// 		document.head.appendChild(script);
+	// 	}
+	// }, [])
 
 	return (
 		<html lang="en">
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9035732498574241"
+				crossOrigin="anonymous"></script>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
