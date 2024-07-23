b63178c49a49ea6e703bbf8cbfcaf64f7904468c
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-07-23 09:14:49 +0530

    blog post design is changed

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar/Navbar";
 import Adsense from "@/components/adSense/Adsense";
 import AddBanner from "@/components/adSense/AddBanner";
+import TagManager from "@/components/tagManager/TagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +15,15 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<Head>
+				<TagManager />
 				<Adsense pId="9035732498574241" />
 			</Head>
 			<body style={{ padding: 0, margin: 0 }} className={inter.className}>
+				<noscript>
+					<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3ZQ2BZS"
+						height="0" width="0"
+						style={{ "display": "none", "visibility": "hidden" }}></iframe>
+				</noscript>
 				<Navbar />
 				{children}
 				<AddBanner dataAdFormat="auto"
