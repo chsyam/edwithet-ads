5d71a0986bb311acadc687876a8607bb447cfacd
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-07-23 07:44:16 +0530

    head tag changed:

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import { useEffect } from "react";
 import Navbar from "@/components/navbar/Navbar";
 import Adsense from "@/components/adSense/Adsense";
 import Head from "next/head";
@@ -14,9 +13,9 @@ export default function RootLayout({ children }) {
 
 	return (
 		<html lang="en">
-			<head>
+			<Head>
 				<Adsense pId="9035732498574241" />
-			</head>
+			</Head>
 			<body style={{ padding: 0, margin: 0 }} className={inter.className}>
 				<Navbar />
 				{children}
