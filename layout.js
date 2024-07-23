e8549142d3f85fd95c15901bb494a8ff9e8a5219
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-07-23 08:44:42 +0530

    blog post design is changed

---
+++ layout.js
@@ -0,0 +1,28 @@
+"use client"
+import "./globals.css";
+import React from "react";
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import Adsense from "@/components/adSense/Adsense";
+import AddBanner from "@/components/adSense/AddBanner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+
+	return (
+		<html lang="en">
+			<Head>
+				<Adsense pId="9035732498574241" />
+			</Head>
+			<body style={{ padding: 0, margin: 0 }} className={inter.className}>
+				<Navbar />
+				{children}
+				<AddBanner dataAdFormat="auto"
+					dataFullWidthResponsive={true}
+					dataAdSlot="9640323625" />
+			</body>
+		</html>
+	);
+}
