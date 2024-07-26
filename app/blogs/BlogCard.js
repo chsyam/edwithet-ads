fe6a121f5a9654f217413435357842bd611761d0
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-07-27 01:20:48 +0530

    backend apis added

--- app/blogs/BlogCard.js
+++ app/blogs/BlogCard.js
@@ -1,25 +1,32 @@
 import Image from "next/image";
 import styles from "./../../styles/Blogs.module.css"
+import BlogImage from "./../../components/images/kfvmyjk6hz8-1536x1051.jpg"
 
 function BlogCard({ blog }) {
+
+    const printFullDate = (date) => {
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var date = new Date(date);
+        return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
 
     return (
         <div className={styles.card}>
             <div>
-                <Image src={blog.image} width={380} height={200} alt="Blog image" />
+                <Image className={styles.image} src={BlogImage} height={200} alt="Blog image" />
             </div>
-            <div className={styles.cardFooter}>
+            <div className={styles.cardFooter} onClick={() => { window.location.href = `/blogs/${blog.id}` }}>
                 <div className={styles.title}>
                     <p>
                         {blog.title}
                     </p>
                 </div>
-                <div className={styles.date}>{blog.dateWritten}</div>
-                <div className={styles.buttons}>
-                    <a href={`/blogs/${blog.id}`}>
-                        Read
-                    </a>
+                <div className={styles.content}>
+                    <p>
+                        {blog.content}
+                    </p>
                 </div>
+                <div className={styles.date}>{printFullDate(blog.postedAt)}</div>
             </div>
         </div>
     )
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,13 +1,27 @@
 "use client";
 import FeedbackForm from "@/app/feedback/FeedbackForm";
-import Feedback from "@/app/feedback/page";
 import UserSvg from "@/components/icons/User";
 import styles from "@/styles/BlogPost.module.css";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function BlogPost({ params }) {
+export default function BlogPost({ params }) {
+
+	const [blogInfo, setBlogInfo] = useState([]);
+
+	const printFormattedDate = (postedAt) => {
+		const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		var date = new Date(postedAt);
+		return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+	}
+
 	useEffect(() => {
-		document.title = "Blogs";
+		async function fetchBlogData() {
+			document.title = "Blogs";
+			const response = await axios.get(`http://localhost:8080/api/blogs/${params.id}`)
+			setBlogInfo(response.data);
+		}
+		fetchBlogData();
 	}, []);
 
 	const blogData = {
@@ -18,26 +32,24 @@ function BlogPost({ params }) {
 		dateWritten: "June 16, 2024",
 		content:
 			"Paul used two metaphors to explain what it means to continue to walk in Jesus. The first metaphor of their faith in Christ was to be “rooted” in Christ. Using an agricultural metaphor of plant growth, Paul reminded them that the basis of their faith, the life that would propel them forward, was found in Jesus. <br > Christ was the source of their strength, spiritual vitality, and ability to please God. He was under the surface in their spiritual lives, nourishing them and helping them to become all that God had for them. <br >It's encouraging to remember that each of us have a “construction zone” sign over our lives. God is actively working on our hearts, building us up, and forming something new in our hearts. We can become discouraged when struggles come, when we lose personal battles over sin, and we find ourselves falling back into old habits. <br >Be encouraged! Christ is living in you, rebuilding your life, and He is your hope of glory (Col 1:27)",
-		videoUrl: "embed/q6OiDvEIgxk?si=neChNHRd076K5bqO",
+		videoUrl: "q6OiDvEIgxk?si=neChNHRd076K5bqO",
 	};
 
 	return (
 		<div className={styles.blogPost}>
-			<div className={styles.title}>{blogData.title}</div>
+			<div className={styles.title}>{/* {blogData.title} */}{blogInfo?.title}</div>
 			<div className={styles.blogDetails}>
 				<div>
 					<UserSvg />
 				</div>
 				<div>
 					<div className={styles.authourName}>{blogData.authourName}</div>
-					<div className={styles.datePublished}>{blogData.dateWritten}</div>
+					<div className={styles.datePublished}>{printFormattedDate(blogInfo?.postedAt)}</div>
 				</div>
 			</div>
 			<div className={styles.resources}>
 				<iframe
-					width="100%"
-					height="100%"
-					src={`https://www.youtube.com/${blogData.videoUrl}`}
+					src={`https://www.youtube.com/embed/${blogInfo?.videoUrl}`}
 					title="YouTube video player"
 					frameBorder="0"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -46,35 +58,17 @@ function BlogPost({ params }) {
 				/>
 			</div>
 			<div className={styles.content}>
-				Paul used two metaphors to explain what it means to continue to walk in
-				Jesus. The first metaphor of their faith in Christ was to be “rooted” in
-				Christ. Using an agricultural metaphor of plant growth, Paul reminded
-				them that the basis of their faith, the life that would propel them
-				forward, was found in Jesus.
-				<br />
-				<br />
-				Christ was the source of their strength, spiritual vitality, and ability
-				to please God. He was under the surface in their spiritual lives,
-				nourishing them and helping them to become all that God had for them.
-				<br />
-				<br />
-				Its encouraging to remember that each of us have a “construction zone”
-				sign over our lives. God is actively working on our hearts, building us
-				up, and forming something new in our hearts. We can become discouraged
-				when struggles come, when we lose personal battles over sin, and we find
-				ourselves falling back into old habits.
+				{blogInfo?.content}
 			</div>
 			<div className={styles.resources}>
 				<iframe
-					src="https://drive.google.com/file/d/1BysRBx7i0J2DFGmd8utTh3nhm0-PcYva/preview"
-					width="100%"
-					height="100%"
+					src={`https://drive.google.com/file/d/${blogInfo?.resourceUrl}/preview`}
 					allow="autoplay"
 				/>
 			</div>
-			<FeedbackForm relatedBlogId={params.id} />
+			<div className={styles.feedbackSection}>
+				<FeedbackForm relatedBlogId={params.id} />
+			</div>
 		</div>
 	);
 }
-
-export default BlogPost;
